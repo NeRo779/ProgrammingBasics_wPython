dec0a1739f33a6d591dbb0898dffadd0a3bc6e43
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-23 16:04:28 +0300

    Update 05_School_Supplies.py

--- First_Steps_In_Coding_Exercise/05_School_Supplies.py
+++ First_Steps_In_Coding_Exercise/05_School_Supplies.py
@@ -1,1 +1,18 @@
+pens_pack_price = 5.80
+marker_pack_price = 7.20
+detergent_price = 1.20
+
+
+pens_pack_amount = int(input())
+marker_pack_amount = int(input())
+detergent = int(input())
+discount_percent = int(input())
+
+total = pens_pack_amount * pens_pack_price \
+    + marker_pack_amount * marker_pack_price \
+    + detergent * detergent_price
+
+total_with_discount = total * (100 - discount_percent) / 100
+
+print(total_with_discount)
 
