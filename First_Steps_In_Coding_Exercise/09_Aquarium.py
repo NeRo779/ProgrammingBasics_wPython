6bd74bce2cb2da753517ec034e8357b1b28ca3f9
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-23 16:09:03 +0300

    Update 09_Aquarium.py

--- First_Steps_In_Coding_Exercise/09_Aquarium.py
+++ First_Steps_In_Coding_Exercise/09_Aquarium.py
@@ -1,1 +1,13 @@
 
+length = int(input())
+width = int(input())
+height = int(input())
+percent = float(input())
+
+aquarium_volume = length * width * height
+volume_in_litres = aquarium_volume * 0.001
+needed_litres = volume_in_litres * (100 - percent) / 100
+
+print(needed_litres)
+
+
