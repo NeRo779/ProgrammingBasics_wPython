3a6904e6bc4f762644a6cb8b8d3f8a4a106a6c6b
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-23 16:02:30 +0300

    Update 03_Deposit_Calculator.py

--- First_Steps_In_Coding_Exercise/03_Deposit_Calculator.py
+++ First_Steps_In_Coding_Exercise/03_Deposit_Calculator.py
@@ -1,1 +1,7 @@
+deposit_amount = float(input()) #200
+deposit_term = int(input()) #3
+annual_interest_rate = float(input()) / 100  #5.7
 
+final_amount = deposit_amount + deposit_term * ((deposit_amount * annual_interest_rate) / 12)
+
+print(final_amount)
