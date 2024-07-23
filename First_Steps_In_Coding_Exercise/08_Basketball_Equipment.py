5e0f6f8b0ea7ebc6d8fd04cd186d3ec7f861f257
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-23 16:10:01 +0300

    Update 08_Basketball_Equipment.py

--- First_Steps_In_Coding_Exercise/08_Basketball_Equipment.py
+++ First_Steps_In_Coding_Exercise/08_Basketball_Equipment.py
@@ -1,1 +1,15 @@
+fee_for_one_year = int(input())
+basketball_sneakers = fee_for_one_year * 0.60
+basketball_outfit = basketball_sneakers * 0.80
+basketball_ball = basketball_outfit * 0.25
+basketball_accessories = basketball_ball * 0.20
+
+fee_for_trainings = fee_for_one_year \
+    + basketball_sneakers \
+    + basketball_outfit \
+    + basketball_ball \
+    + basketball_accessories
+
+print(fee_for_trainings)
+
 
