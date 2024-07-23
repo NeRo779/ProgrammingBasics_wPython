21b8776ec815b9343fff6cf2c3ad9f9de042480f
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-23 16:03:35 +0300

    Update 04_Required_reading.py

--- First_Steps_In_Coding_Exercise/04_Required_reading.py
+++ First_Steps_In_Coding_Exercise/04_Required_reading.py
@@ -1,1 +1,8 @@
+pages_amount = int(input())
+pages_per_hour = int(input())
+days_amount = int(input())
 
+total_hours = pages_amount // pages_per_hour
+hours_per_day = total_hours / days_amount
+
+print(hours_per_day)
