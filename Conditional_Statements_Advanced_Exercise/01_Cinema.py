6341dc41727d4d3208494dc0a78c564f13688cf9
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-07-28 14:02:03 +0300

    Update 01_Cinema.py

--- Conditional_Statements_Advanced_Exercise/01_Cinema.py
+++ Conditional_Statements_Advanced_Exercise/01_Cinema.py
@@ -1,1 +1,21 @@
+Premiere = 12.00
+Normal = 7.50
+Discount = 5.00
+
+projection = str(input())
+rows = int(input())
+columns = int(input())
+income = 0
+
+capacity = rows * columns
+
+if projection == "Premiere":
+    income = capacity * 12.00
+elif projection == "Normal":
+    income = capacity * 7.50
+elif projection == "Discount":
+    income = capacity * 5.00
+
+print(f"{income:.2f} leva")
+
 
